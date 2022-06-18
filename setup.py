80eb1d8d0242d74388ec841b4438141096285510
matius300 <33297541+matius300@users.noreply.github.com>
2022-06-19 04:51:19 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "remocolab.py",
     version = "0.1",
-    py_modules = ['azazil'],
+    py_modules = ['remocolab'],
     url = "https://github.com/matius300/remocolab.xrdp",
     author = "azazil",
     install_requires = ["pyngrok"]
